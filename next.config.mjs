b8c87f60dfba73e2602c0de19a4e535006b214e9
LaneLR <laneleerichardson@gmail.com>
2025-06-23 11:11:24 -0500

    config adjustments and added dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    styledComponents: true, //enable the styled-components SWC compiler
+  },
+  //   devIndicators: {
+  //   buildActivity: false,
+  // },
+};
 
 export default nextConfig;
