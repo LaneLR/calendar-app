b3a2d6150d48003bea28fddc583a2b05c9df718f
LaneLR <laneleerichardson@gmail.com>
2025-06-24 16:10:17 -0500

    deployment testing

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -6,17 +6,12 @@ export async function POST(req) {
     console.log("Login route triggered");
 
     const db = await initializeDbAndModels();
-    console.log("DB initialized");
-
     const User = db.User;
 
     const body = await req.json();
-    console.log("Request body:", body);
-
     const { username, password } = body;
 
     if (!username || !password) {
-      console.log("Missing username or password");
       return NextResponse.json(
         { error: "Must have a username or password" },
         { status: 400 }
@@ -24,26 +19,19 @@ export async function POST(req) {
     }
 
     const user = await User.findOne({ where: { username } });
-    console.log("User found:", user ? user.id : "Not found");
-
     if (!user) {
       return NextResponse.json({ error: "User doesn't exist" });
     }
 
     const isPasswordValid = await user.validatePassword(password);
-    console.log("Password valid:", isPasswordValid);
-
-    console.log("DEBUG: This is the user:", user)
-
     if (!isPasswordValid) {
       return NextResponse.json({ error: "Wrong password" });
     }
 
-
     const { password: _, ...userWithoutPassword } = user.toJSON();
-    console.log("DEBUG: this is the user without password", userWithoutPassword)
 
-    return NextResponse.json({
+    console.log("Returning success response");
+    return await NextResponse.json({
       message: "Login successful",
       user: userWithoutPassword,
     });
@@ -52,4 +40,3 @@ export async function POST(req) {
     return NextResponse.json({ error: "Internal error" }, { status: 500 });
   }
 }
-
