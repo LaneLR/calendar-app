52cf95e49f3cc09e2b2937df59ac8d0db39b8e19
LaneLR <laneleerichardson@gmail.com>
2025-06-24 16:04:16 -0500

    add debugging logs

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -33,11 +33,15 @@ export async function POST(req) {
     const isPasswordValid = await user.validatePassword(password);
     console.log("Password valid:", isPasswordValid);
 
+    console.log("DEBUG: This is the user:", user)
+
     if (!isPasswordValid) {
       return NextResponse.json({ error: "Wrong password" });
     }
 
+
     const { password: _, ...userWithoutPassword } = user.toJSON();
+    console.log("DEBUG: this is the user without password", userWithoutPassword)
 
     return NextResponse.json({
       message: "Login successful",
