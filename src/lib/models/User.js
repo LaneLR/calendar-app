fa85cd49c32dfe527eb329c2a43fa76e6a4c5bde
LaneLR <laneleerichardson@gmail.com>
2025-06-24 12:31:40 -0500

    add hashing to password

--- src/lib/models/User.js
+++ src/lib/models/User.js
@@ -5,6 +5,11 @@ class User extends Model {
   async validatePassword(password) {
     return bcrypt.compare(password, this.password);
   }
+
+  static async hashPassword(password) {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
+  }
 }
 
 export default User;
