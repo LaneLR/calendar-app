d657ed94bf6436cb4a61e21b99655cd464aefb34
LaneLR <laneleerichardson@gmail.com>
2025-06-26 17:26:14 -0500

    fix conditional rendering

--- src/components/DetailsContactTab.jsx
+++ src/components/DetailsContactTab.jsx
@@ -67,7 +67,9 @@ const ContactFunctionWrapper = styled.div`
 export default function DetailsContactTab({ contact }) {
   const trashcanImage = "/images/trashcan.png";
 
-  const { result, addContacts,  } = useCalendar();
+  const { contacts, addContacts, deleteContact } = useCalendar();
+  const contactExists = contacts.some((c) => c.id === contact.id);
+
 
   return (
     <TabWrapper>
@@ -80,22 +82,29 @@ export default function DetailsContactTab({ contact }) {
         <ContactSnippet>{contact.username}</ContactSnippet>
       </ContactSnippetWrapper>
 
-      {result.length < 3 ? (
-        <AddContactButton onClick={() => addContacts(contact.id)}>+</AddContactButton>
+      {!contactExists ? (
+        <AddContactButton onClick={() => addContacts(contact.id)}>
+          +
+        </AddContactButton>
       ) : (
         <>
           <ContactFunctionWrapper>
-            <Image
-              src={trashcanImage}
-              width={25}
-              height={50}
-              alt="Delete contact icon"
-              style={{
-                margin: "0 10px",
-                objectFit: "contain",
-                cursor: "pointer",
-              }}
-            />
+            <button
+              style={{ border: "none", backgroundColor: "rgba(0,0,0,0)" }}
+              onClick={() => deleteContact(contact.id)}
+            >
+              <Image
+                src={trashcanImage}
+                width={25}
+                height={50}
+                alt="Delete contact icon"
+                style={{
+                  margin: "0 10px",
+                  objectFit: "contain",
+                  cursor: "pointer",
+                }}
+              />
+            </button>
           </ContactFunctionWrapper>
         </>
       )}
