66ccd1a1d81137c9f979560cdaccab9fd1c53cf7
LaneLR <laneleerichardson@gmail.com>
2025-07-01 11:16:37 -0500

    style changes to delete modal

--- src/components/DeleteEventModal.jsx
+++ src/components/DeleteEventModal.jsx
@@ -2,57 +2,200 @@
 "use client";
 import styled from "styled-components";
 import ModalButton from "./ModalButton"; // or use your existing styled button
+import { useEffect, useState } from "react";
 
-const Overlay = styled.div`
+const ModalWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+const ModalForm = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 70%;
+  max-width: ;
+`;
+
+const ModalOverlay = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  z-index: 20;
-  width: 100vw;
-  height: 100vh;
-  background: rgba(0, 0, 0, 0.6);
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 10;
 `;
 
-const ModalContainer = styled.div`
-  background-color: var(--color-modal-bg);
-  color: var(--color-modal-text);
-  width: 90%;
-  max-width: 500px;
-  padding: 20px;
-  border-radius: 10px;
-  margin: 100px auto;
-`;
-
-const UserList = styled.ul`
-  padding: 0;
-  margin: 10px 0 20px 0;
-  list-style: none;
-`;
-
-const ButtonGroup = styled.div`
+const ModalButtonWrapper = styled.div`
   display: flex;
   justify-content: space-evenly;
-  gap: 20px;
+  align-items: center;
+  width: 100%;
+`;
+
+const ModalFormContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-flow: column nowrap;
+  background-color: white;
+  height: auto;
+  width: 60%;
+  padding: 30px;
+  border-radius: 10px;
+  min-width: 315px;
+  max-width: 450px;
+`;
+
+const TextContainerLeftAlign = styled.div`
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  text-align: left;
+  width: 80%;
+  min-width: 100px;
+`;
+
+const TitleBar = styled.input`
+  width: 80%;
+  font-size: 1.1rem;
+  margin: 0 0 20px 0;
+`;
+
+const AddDatesContainers = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+  flex-flow: column nowrap;
+`;
+
+const ContactsAddedContainer = styled.div`
+  width: 80%;
+  height: auto;
+  background-color: lightgray;
+  margin: 0 0 20px 0;
+`;
+
+const TextContainer = styled.div`
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default function DeleteEventModal({ event, onCancel, onConfirm }) {
-  return (
-    <Overlay>
-      <ModalContainer>
-        <h3>Delete Event</h3>
-        <p><strong>Title:</strong> {event.title}</p>
-        <p><strong>Invited Users:</strong></p>
-        <UserList>
-          {event.users?.map((u, i) => (
-            <li key={i}>{u.username || u.name || `User ID ${u}`}</li>
-          ))}
-        </UserList>
+    const [users, setUsers] = useState([])
 
-        <ButtonGroup>
-          <ModalButton onClick={onCancel}>Cancel</ModalButton>
-          <ModalButton onClick={() => onConfirm(event.id)}>Delete</ModalButton>
-        </ButtonGroup>
-      </ModalContainer>
-    </Overlay>
+  useEffect(() => {
+    if (!event) return;
+
+    if (Array.isArray(event.users)) {
+      setUsers(event.users);
+    } else if (Array.isArray(event.userIds)) {
+      // fallback if users array not included
+      fetch("/api/users/byIds", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ ids: event.userIds }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.users) setUsers(data.users);
+        })
+        .catch((err) => console.error("Failed to fetch users", err));
+    }
+  }, [event]);
+
+  return (
+    <>
+      <ModalOverlay>
+        <ModalWrapper>
+          <ModalForm>
+            <ModalFormContainer>
+              <TextContainer>
+                <TextContainerLeftAlign>
+                  <h4 style={{ paddingBottom: "5px" }}>Event Title</h4>
+                </TextContainerLeftAlign>
+                <h4
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    width: "100%",
+                  }}
+                >
+                  {/* {event.title} */}
+                  Event Name
+                </h4>
+                <AddDatesContainers>
+                  <div>
+                    <TextContainerLeftAlign>
+                      <h4 style={{ padding: "5px 0" }}>Start Time:</h4>
+                    </TextContainerLeftAlign>
+                    <p
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        width: "100%",
+                      }}
+                    >
+                      {/* {event.start} */}
+                      11:30:00pm
+                    </p>
+                  </div>
+                  <div>
+                    <TextContainerLeftAlign>
+                      <h4 style={{ padding: "5px 0" }}>End Time:</h4>
+                    </TextContainerLeftAlign>
+                    <p
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        width: "100%",
+                      }}
+                    >
+                      {/* {event.end} */}
+                      10:30:00pm
+                    </p>
+                  </div>
+                </AddDatesContainers>
+                <TextContainerLeftAlign>
+                  <h4 style={{ width: "100%", padding: "5px 0" }}>
+                    Invited Contacts
+                  </h4>
+                </TextContainerLeftAlign>
+
+                <ContactsAddedContainer>
+                  {users?.length > 0 ? (
+                    event?.map((user) => (
+                      <UserOnEventInModal key={user.id} user={user} />
+                    ))
+                  ) : (
+                    <p
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        width: "100%",
+                        backgroundColor: '#fff',
+                      }}
+                    >
+                    No invited contacts
+                    </p>
+                  )}
+                </ContactsAddedContainer>
+                <ModalButtonWrapper>
+                  <ModalButton onClick={onConfirm}>Delete</ModalButton>
+                  <ModalButton onClick={onCancel}>Cancel</ModalButton>
+                </ModalButtonWrapper>
+              </TextContainer>
+            </ModalFormContainer>
+          </ModalForm>
+        </ModalWrapper>
+      </ModalOverlay>
+    </>
   );
 }
