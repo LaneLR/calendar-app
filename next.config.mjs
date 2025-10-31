7d8cb748eb9ab6f99665a51797a05b1bdc4fdf8d
LaneLR <laneleerichardson@gmail.com>
2025-10-31 13:03:34 -0500

    Update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true, //enable the styled-components SWC compiler
   },
-  //   devIndicators: {
-  //   buildActivity: false,
-  // },
-  output: 'export',
-  distDir: 'out',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
